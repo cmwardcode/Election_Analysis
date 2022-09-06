c845d98ad8ee664fa70f10b1ee4757b34a714717
cmwardcode <110561011+cmwardcode@users.noreply.github.com>
2022-09-05 21:30:03 -0700

    Adding PyPoll.py file.

---
+++ Practice.py
@@ -0,0 +1,15 @@
+#Empty Dictionary Creation
+counties_dict = {}
+
+#Adding keys and values to dictionary, county is key and value is num of reg voters
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+
+#Empty List of Dictionaries
+voting_data = []
+
+#Each element in the list is a dictionary
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
